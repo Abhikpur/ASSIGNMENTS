50e1336a74615c3b6362b929b94abe0af66556ad
Abhikpur <65890173+Abhikpur@users.noreply.github.com>
2022-09-07 23:06:41 +0530

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Calculator
+{
+    public static void Main()
+    {
+        string s;
+        do
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine("Enter 1st no");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd no");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a for addition/nEnter b for substraction/n" +
+                "Enter c for multiplication/nEnter d for division ");
+            char x = char.Parse(Console.ReadLine());
+            if (x == 'a')
+                Console.WriteLine("Addition = " + (a + b));
+            else if (x == 'b')
+                Console.WriteLine("Substarction = " + Math.Abs(a - b));
+            else if (x == 'c')
+                Console.WriteLine("Multiplication = " + (a * b));
+            else if (x == 'd')
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("2nd no cannot be zero");
+                }
+
+                else
+                {
+                    float f = a / b;
+                    Console.WriteLine("Division = " + Convert.ToString(f));
+                }
+            }
+            else
+                Console.WriteLine("Enter valid character");
+            Console.WriteLine("Do you want to do again ? write Yes or No");
+             s = Console.ReadLine();
+        }
+        while (s.ToUpper() == "YES");
+
+        Console.WriteLine("Thank you for using the Calculator");
+    }
+}
